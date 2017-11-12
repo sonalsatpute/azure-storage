1d2ee9635054e1449e815a188c74278226fdd656
sonalsatpute <sonal.satpute@gmail.com>
2017-11-12 15:59:52 +0530

    added smaple opration for Blob

--- azure-storage/AzureStorage/BlobStorage.cs
+++ azure-storage/AzureStorage/BlobStorage.cs
@@ -6,24 +6,85 @@ using System.IO;
 
 namespace AzureStorage
 {
-  public class BlobStorage
+  public class AzureBlobStorageContainer
   {
     private readonly string _storageAccountName;
     private readonly string _storageAccountKey;
+    private readonly string _containerName;
+    private CloudBlobContainer _container;
 
-    public BlobStorage(string storageAccountName, string storageAccountKey)
+    public AzureBlobStorageContainer(string storageAccountName, string storageAccountKey, string containerName)
     {
       _storageAccountName = storageAccountName;
       _storageAccountKey = storageAccountKey;
+      _containerName = containerName;
     }
 
-    public void CreateContainer(string containerName)
+    public void CreateIfNotExists(string containerName)
     {
       var storageCredentials = new StorageCredentials(_storageAccountName, _storageAccountKey);
       var cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
       var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
-      var container = cloudBlobClient.GetContainerReference(containerName);
-      container.CreateIfNotExistsAsync();
+      _container = cloudBlobClient.GetContainerReference(containerName);
+      _container.CreateIfNotExistsAsync();
+    }
+
+    public void ListAttributes()
+    {
+      _container.FetchAttributesAsync();
+      Console.WriteLine("Container name " + _container.StorageUri.PrimaryUri.ToString());
+      Console.WriteLine("Last modified " + _container.Properties.LastModified.ToString());
+    }
+
+    public void SetMetadata()
+    {
+      _container.Metadata.Clear();
+      _container.Metadata.Add("author", "sonal satpute");
+      _container.Metadata["authoredOn"] = DateTime.Now.ToString();
+      _container.SetMetadataAsync();
+    }
+
+    public void ListMetadata()
+    {
+      Console.WriteLine("Metadata:\n");
+      foreach (var item in _container.Metadata)
+      {
+        Console.WriteLine("Key " + item.Key);
+        Console.WriteLine("Value " + item.Value + "\n\n");
+      }
+    }
+
+    public void UploadBlob(string path)
+    {
+      FileInfo info = new FileInfo(path);
+      CloudBlockBlob blockBlob = _container.GetBlockBlobReference(info.Name);
+
+      using (var fileStream = File.OpenRead(path))
+      {
+        blockBlob.UploadFromStreamAsync(fileStream);
+      }
+    }
+
+    public void CopyBlob(string blobName, string newBlobName)
+    {
+      CloudBlockBlob blockBlob = _container.GetBlockBlobReference(blobName);
+      CloudBlockBlob copyToBlockBlob = _container.GetBlockBlobReference(newBlobName);
+      copyToBlockBlob.StartCopyAsync(new Uri(blockBlob.Uri.AbsoluteUri));
+    }
+
+    public void UploadBlobSubdirectory(string path)
+    {
+      FileInfo info = new FileInfo(path);
+
+      CloudBlobDirectory directory = _container.GetDirectoryReference("parent-directory");
+      CloudBlobDirectory subdirectory = directory.GetDirectoryReference("child-directory");
+      CloudBlockBlob blockBlob = subdirectory.GetBlockBlobReference(info.Name);
+      
+      using (var fileStream =File.OpenRead(path))
+      {
+        blockBlob.UploadFromStreamAsync(fileStream);
+      }
+
     }
   }
 }
--- azure-storage/AzureStorage/Program.cs
+++ azure-storage/AzureStorage/Program.cs
@@ -16,8 +16,14 @@ namespace AzureStorage
 
       string storageAccountName = Configuration["StorageAccountName"];
       string storageAccountKey = Configuration["StorageAccountKey"];
-      BlobStorage blobStorage = new BlobStorage(storageAccountName, storageAccountKey);
-      blobStorage.CreateContainer("new-container");
+      AzureBlobStorageContainer container = new AzureBlobStorageContainer(storageAccountName, storageAccountKey, "new-container");
+      container.CreateIfNotExists("new-container");
+      container.ListAttributes();
+      container.SetMetadata();
+      container.ListMetadata();
+      container.UploadBlob("532_OD_Changes.pdf");
+      container.CopyBlob("532_OD_Changes.pdf", "Copy_Of_532_OD_Changes.pdf");
+      container.UploadBlobSubdirectory("532_OD_Changes.pdf");
 
       Console.WriteLine("Press any key to exit.");
       Console.Read();
